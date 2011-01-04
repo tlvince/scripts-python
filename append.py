fa84bd7bb452a786c2947a4b6bf77b1e62512233
Tom Vincent <thomasleevincent@hsbc.com.hk>
2011-01-04 16:37:10 +0800

    Import the others

---
+++ append.py
@@ -0,0 +1,13 @@
+import sys
+import io
+
+def append(file, arr):
+    str = ' '.join(arr)
+    with open(file, mode='a+') as theFile:
+        theFile.write(str + '\n')
+
+def main():
+    append(sys.argv[1], sys.argv[2:])
+
+if __name__ == '__main__':
+    main()
