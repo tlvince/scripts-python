dbf31d2dd8c69e60be2af962030c0cd930a1c5ac
Tom Vincent <tlvince+linux@gmail.com>
2011-03-27 20:10:20 +0800

    Specialise to file names

--- septicise.py
+++ septicise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2011 Tom Vincent <http://www.tlvince.com/contact/>
 
-"""Convert a pathname to title case."""
+"""Convert a filename to title case."""
 
 import argparse
 import string
@@ -10,17 +10,18 @@ import os.path
 def parseArguments():
     """Parse the command-line arguments."""
     parser = argparse.ArgumentParser(description=__doc__.split("\n")[0])
-    parser.add_argument("path", nargs="+", help="the path(s) to convert")
+    parser.add_argument("file", nargs="+", help="the file(s) to convert")
     return parser.parse_args()
 
 def main():
     """Start execution of septicise."""
     args = parseArguments()
-    paths = args.path
-    paths = [p.replace("-", " ") for p in paths]        # Dash -> space
-    paths = [string.capwords(p, " ") for p in paths]    # Title case words
-    paths = [os.path.splitext(p)[0] for p in paths]     # Remove extension
-    [print(p) for p in paths]
+    files = args.file
+    files = [os.file.basename(f) for f in files]        # Get files
+    files = [f.replace("-", " ") for f in files]        # Dash -> space
+    files = [string.capwords(f, " ") for f in files]    # Title case words
+    files = [os.file.splitext(f)[0] for f in files]     # Remove extension
+    [print(f) for f in files]
 
 if __name__ == "__main__":
     main()
