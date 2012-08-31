a0e4632d1166a8cf672974709b832d9a7d015963
Tom Vincent <git@tlvince.com>
2012-08-31 21:17:28 +0800

    Document parse_args, pep check

--- periodical.py
+++ periodical.py
@@ -17,7 +17,7 @@ from sys import exit
 
 from boilerpipe.extract import Extractor
 
-app = "periodical"
+APP = "periodical"
 
 def write_html(config, path, urls):
     """Generate stripped HTML file for the given URL."""
@@ -39,7 +39,7 @@ def write_html(config, path, urls):
 def write_yaml(config, mobi, path, date):
     """Write document YAML."""
     doc = {
-        "doc_uuid":     "{0}-{1}".format(app, date.strftime("%Y%m%d%H%M%S")),
+        "doc_uuid":     "{0}-{1}".format(APP, date.strftime("%Y%m%d%H%M%S")),
         "title":        "{0} {1}".format(config.get("meta", "title"),
                                          date.strftime("%Y-%m-%d")),
         "author":       config.get("meta", "author"),
@@ -82,17 +82,17 @@ def log(path, urls, date):
         for url in urls:
             logfile.write("{0} {1}\n".format(date.strftime("%Y%m%d"), url))
 
-def parse_args(config, log, url):
+def parse_args(config_path, log_path, urls_path):
     """Parse the command-line arguments."""
     parser = argparse.ArgumentParser(description=__doc__.split("\n")[0],
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("url", help="the article URL", nargs="*")
-    parser.add_argument("-c", "--config", default=config,
+    parser.add_argument("url", help="the webpage URL", nargs="*")
+    parser.add_argument("-c", "--config", default=config_path,
         help="path to config file")
-    parser.add_argument("-l", "--log", default=log,
+    parser.add_argument("-l", "--log", default=log_path,
         help="path to log file")
-    parser.add_argument("-f", "--file", default=url, nargs="?",
-        help="path to a file containing URLs")
+    parser.add_argument("-f", "--file", default=urls_path, nargs="?",
+        help="path to a file containing a newline seperated list of URLs")
     return parser.parse_args()
 
 def main():
@@ -100,13 +100,13 @@ def main():
     config_path = os.getenv("XDG_CONFIG_HOME")
     if not config_path:
         config_path = os.path.join(os.getenv("HOME"), ".config")
-    config_path = os.path.join(config_path, app, app + ".conf")
+    config_path = os.path.join(config_path, APP, APP + ".conf")
 
     data_path = os.getenv("XDG_DATA_HOME")
     if not data_path:
         data_path = os.path.join(os.getenv("HOME"), ".local", "share")
-    log_path = os.path.join(data_path, app, app + ".log")
-    url_path = os.path.join(data_path, app, app + "-urls.txt")
+    log_path = os.path.join(data_path, APP, APP + ".log")
+    url_path = os.path.join(data_path, APP, APP + "-urls.txt")
     
     args = parse_args(config_path, log_path, url_path)
     if not args.url:
@@ -123,7 +123,7 @@ def main():
 
     tmp = tempfile.mkdtemp()
     date = datetime.datetime.now()
-    mobi = "{0}-{1}.mobi".format(app, date.strftime("%Y%m%d%H%M%S"))
+    mobi = "{0}-{1}.mobi".format(APP, date.strftime("%Y%m%d%H%M%S"))
 
     write_yaml(config, mobi, tmp, date)
     write_html(config, tmp, args.url)
